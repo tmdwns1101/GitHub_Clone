d053a4e7a1c438db53963ca8fe1880ebec0740dd
tmdwns1101 <tmdwns1101@naver.com>
2020-03-02 00:06:37 +0900

    init commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $("#nav_bar").load("/layouts/navigation.html");
+
+    
+})
---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".logo").click(function(){
+        alert("logo is clicked");
+    })
+
+    $(".main-menu").click(function(){
+        alert("menu is Clicked!");
+    })
+})
